ef0be74df10b1b54b66b08d37fa7a9d6d86f8d6b
Gjelbrim Klobocishta <jebklob01@gmail.com>
2022-12-13 02:30:43 -0500

    template for loading api complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import axios from "axios";
+import Loading from './components/Loading';
+import Emp from './components/Emp';
+import Emplist from './components/Emplist';
+import Issues from './components/Issues';
+
 
 function App() {
+
+  const [loading, setloading] = useState(true);
+  const [emp, setemp] = useState([]);
+
+
+  const getEmp = () => {
+    setloading(true);
+    axios.get('https://api.matgargano.com/employees/').then(response => {
+      setemp(response.data);
+      setloading(false);
+    })
+    
+  };
+
+  useEffect(() => {
+    getEmp();
+  }, []);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Container'>
+      {!!loading && <Loading/>}
+      {!loading && (
+        <div className='Content'>
+        < Emplist data = {emp} />
+        < Emp/>
+        < Issues />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/Emplist.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Emplist = props => {
+  return (
+    <div>Emplist</div>
+  )
+}
+
+Emplist.propTypes = {}
+
+export default Emplist
